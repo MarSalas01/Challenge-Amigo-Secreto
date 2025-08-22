a42f397ffada6952b9825a90c153de69024b2a99
Marcela Salas <marsalas1488@gmail.com>
2025-08-22 18:59:16 -0500

    Agrega funcionalidad

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigos = [];
+const input = document.querySelector('#amigo');
+const listaAmigos = document.querySelector('#listaAmigos');
+const resultadoElement = document.querySelector('#resultado');
+
+
+function agregarAmigo(){
+    const nombre = input.value.trim();
+
+    if(nombre){
+        amigos.push(nombre);
+        actualizarLista();
+    } else {
+        alert('Por Favor inserte un nombre');
+    }
+
+    input.value = '';
+    input.focus();
+}
+
+function actualizarLista(){
+    listaAmigos.innerHTML = '';
+
+    for(let i = 0; i < amigos.length; i++) {
+        const listElement = document.createElement('li');
+        listElement.textContent = amigos[i];
+        listaAmigos.appendChild(listElement);
+        console.log(listElement);
+    }
+}
+
+function sortearAmigo() {
+    const numeroMaximoPosible = amigos.length;
+    const numeroAleatorio = Math.floor(Math.random()*numeroMaximoPosible);
+    const amigoSecreto = amigos[numeroAleatorio];
+    console.log(numeroAleatorio);
+
+    if(amigos.length){
+        console.log(amigoSecreto);
+
+        resultadoElement.innerHTML = `Tu amigo secreto es ${amigoSecreto}`;
+    } else {
+        alert('Debes agregar amigos primero');
+    }
+}
